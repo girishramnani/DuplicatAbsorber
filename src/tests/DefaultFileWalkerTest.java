2f338402c10271ede604172294c272471b038a68
girishramnani <girishramnani95@gmail.com>
2015-03-10 07:37:16 +0530

    File Walker Tests

---
+++ src/tests/DefaultFileWalkerTest.java
@@ -0,0 +1,37 @@
+package tests;
+
+import FileIO.DefaultFileWalker;
+import FileIO.FileEvent;
+import FileIO.FileEventListener;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.*;
+
+public class DefaultFileWalkerTest {
+    DefaultFileWalker df;
+    @Before
+    public void setUp() throws Exception {
+         df = new DefaultFileWalker();
+        df.setFileEventListener(new FileEventListener() {
+            @Override
+            public void apply(FileEvent fileEvent) {
+                System.out.println(fileEvent.getPath().getFileName());
+            }
+        });
+
+
+    }
+    @Test
+    public void test_name(){
+        try {
+            Files.walkFileTree(Paths.get("../"),df);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
