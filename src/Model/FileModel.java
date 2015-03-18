a1b6ac6cfa4890807467e23354aebc08157503c6
girishramnani <girishramnani95@gmail.com>
2015-03-18 23:29:45 +0530

    added classes for better rendering

--- src/Model/FileModel.java
+++ src/Model/FileModel.java
@@ -5,6 +5,7 @@ import FileFind.FileEvent;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,11 +16,14 @@ public class FileModel extends AbstractTableModel {
     private List<FileEvent> filecolumn;
     private List<Boolean> selection;
     private String[] headers ={"id","File","Selected"};
+    private Class[] classes = {int.class,String.class,Boolean.class};
     private int index=1;
     public FileModel(){
         super();
         filecolumn=new ArrayList<>();
         selection= new ArrayList<>();
+        filecolumn.add(new FileEvent(Paths.get(".")));
+        selection.add(Boolean.FALSE);
     }
 
     @Override
@@ -37,17 +41,23 @@ public class FileModel extends AbstractTableModel {
         return filecolumn.size();
     }
 
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        return classes[columnIndex];
+    }
+
     @Override
     public Object getValueAt(int row, int column) {
         if(column ==0){
             return row;
         }
         else if(column ==1){
-            return filecolumn.get(row);
+            return filecolumn.get(row).getFile().toString();
 
         }
         else{
-            return selection.get(column);
+            return selection.get(row);
         }
     }
 }
