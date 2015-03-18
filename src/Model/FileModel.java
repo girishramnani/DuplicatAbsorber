90b01ccfc7ff2f5c2615f54c0dc0d9038b67e124
girishramnani <girishramnani95@gmail.com>
2015-03-18 23:04:02 +0530

    added file model

--- src/Model/FileModel.java
+++ src/Model/FileModel.java
@@ -1,9 +1,51 @@
 package Model;
 
+import FileFind.FileEvent;
+
 import javax.swing.table.DefaultTableModel;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Girish on 18-03-2015.
  */
 public class FileModel extends DefaultTableModel {
+    private List<FileEvent> filecolumn;
+    private List<Boolean> selection;
+    private String[] headers ={"id","File","Selected"};
+    private int index=1;
+    public FileModel(){
+        filecolumn = new ArrayList<>();
+        selection = new ArrayList<>();
+
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return headers[column];
+    }
+
+    @Override
+    public int getColumnCount() {
+        return headers.length;
+    }
+
+    @Override
+    public int getRowCount() {
+        return filecolumn.size();
+    }
+
+    @Override
+    public Object getValueAt(int row, int column) {
+        if(column ==0){
+            return row;
+        }
+        else if(column ==1){
+            return filecolumn.get(row);
+
+        }
+        else{
+            return selection.get(column);
+        }
+    }
 }
