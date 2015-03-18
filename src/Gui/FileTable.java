77a5f55f2a70e567b71ae80a9cdeddb55f8397c1
girishramnani <girishramnani95@gmail.com>
2015-03-18 23:04:27 +0530

    added filemodel to the jTable

--- src/Gui/FileTable.java
+++ src/Gui/FileTable.java
@@ -1,14 +1,22 @@
 package Gui;
 
 
+import FileFind.FileEvent;
+import Model.FileModel;
+
 import javax.swing.*;
+import java.io.File;
+import java.util.List;
 
 /**
  * the main File Jtable that shows the files which are duplicate.
  */
 public class FileTable extends JTable {
+
     public FileTable(){
         super();
+        setModel(new FileModel());
+
 
     }
 }
