c9ce0f4fffa249f5d15da2bd733563f058f0c02b
girishramnani <girishramnani95@gmail.com>
2015-03-18 23:41:06 +0530

    file name testing

---
+++ src/tests/ProgressDialog.java
@@ -0,0 +1,20 @@
+package tests;
+
+import Gui.FileProgressBar;
+
+/**
+ * Created by Girish on 18-03-2015.
+ */
+public class ProgressDialog {
+    public static void main(String[] args) {
+        FileProgressBar fpb = new FileProgressBar();
+        try {
+            for(int i=0;i<10;i++){
+                Thread.sleep(1000);
+                fpb.setFileName("the "+i+" th second");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
