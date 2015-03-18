7abae75fa44b744eee88d48a730fb30a2ed74bdd
girishramnani <girishramnani95@gmail.com>
2015-03-18 14:20:24 +0530

    added top panel

--- src/Gui/TopPanel.java
+++ src/Gui/TopPanel.java
@@ -1,7 +1,19 @@
 package Gui;
 
+import javax.swing.*;
+
 /**
  * Created by Girish on 17-03-2015.
  */
-public class TopPanel {
+public class TopPanel extends JPanel {
+    private JLabel label ;
+    private JTextField directoryField;
+    private JButton BrowseButton;
+
+    public TopPanel(){
+        label = new JLabel("The directory:-");
+        directoryField = new JTextField(20);
+
+
+    }
 }
