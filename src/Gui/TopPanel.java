3e8f5bf7df65208361a780a01e2d741a3bcca47e
girishramnani <girishramnani95@gmail.com>
2015-03-18 20:26:13 +0530

    added more methods

--- src/Gui/TopPanel.java
+++ src/Gui/TopPanel.java
@@ -1,16 +1,28 @@
 package Gui;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.io.File;
 
 /**
  * Created by Girish on 17-03-2015.
  */
 public class TopPanel extends JPanel {
     private JLabel label ;
+    private File selectedDirectory;
     private JTextField directoryField;
     private JButton BrowseButton;
     private LayoutManager layoutManager;
+    private String Filename;
+    private Interactor interactor;
+    public String getFilename() {
+        return Filename;
+    }
+
+    public void setInteractor(Interactor interactor) {
+        this.interactor = interactor;
+    }
 
     public TopPanel(){
         layoutManager = new BoxLayout(this,BoxLayout.X_AXIS);
@@ -28,4 +40,27 @@ public class TopPanel extends JPanel {
 
 
     }
+    public void FileSelection(){
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int a=fileChooser.showOpenDialog(this);
+        if(a ==JFileChooser.APPROVE_OPTION){
+            selectedDirectory=fileChooser.getSelectedFile();
+            //check for the directory permissions
+            if(interactor !=null){
+                interactor.interact();
+            }
+        }
+
+
+
+
+
+    }
+
+    private boolean _validate(){
+        return false;
+    }
+
+
 }
