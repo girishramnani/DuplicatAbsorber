5ebd4c05a292c96ab4750404952e03e118c43787
girishramnani <girishramnani95@gmail.com>
2015-03-18 10:34:56 +0530

    added file table

---
+++ src/Gui/FileTable.java
@@ -0,0 +1,14 @@
+package Gui;
+
+
+import javax.swing.*;
+
+/**
+ * the main File Jtable that shows the files which are duplicate.
+ */
+public class FileTable extends JTable {
+    public FileTable(){
+        super();
+
+    }
+}
